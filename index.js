496473112aba7753b8eee6114b007a416d0724d6
BagusTaqwillah <bagustaqwillah0302@gmail.com>
2023-06-02 19:48:50 +0700

    update routing

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ require("dotenv").config()
 app.listen(process.env.PORT,()=>{
     console.log(`server berjalan  pada port ${process.env.PORT}`)
 })
+app.get("/",(req,res)=>{
+    res.json({
+        message:"RESTFULL API E-BOOK"
+    })
+})
 const authRouter=require("./routes/authRoutes")
 const bookRouter=require("./routes/bookRoutes")
 app.use(authRouter)
